7e3f6476fdb380b0846b70f6cf35bc17f85bb01b
James Buchanan <jbuchanan@newrelic.com>
2024-10-08 12:18:29 +0100

    query handler exmaple

--- visualizations/example-viz/components/ExampleComponent.tsx
+++ visualizations/example-viz/components/ExampleComponent.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { useProps } from "../context/VizPropsProvider";
+import { useNerdGraphQuery } from "../hooks/useNerdGraphQuery";
 
 const ExampleComponent = () => {
   const vizProps = useProps();
-  const { greeting } = vizProps;
+  const { greeting, accountId, query } = vizProps;
+  
+  let data;
+  if(query && query!="" && accountId && accountId!=""){ 
+    ({ data } = useNerdGraphQuery(accountId, query));
+  }
   return (
+        <>
         <div>{greeting}</div>
+        <div>AccountID: {accountId=="" ? "Not specified" : accountId} Results: {data ? data.length: "Loading..."}</div>
+        </>
   );
 };
 
